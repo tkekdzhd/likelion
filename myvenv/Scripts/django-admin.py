337c7ae7c7e60679b35a00aabbb2acc88e769282
tkekdzhd <tkekdzhd@gmail.com>
2019-03-28 20:17:00 +0900

    first

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tkekd\onedrive\바탕 화면\startbootstrap-creative-gh-pages\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
